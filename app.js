70b6b9a1b90d3bb4cf838e81b14b5555c2fd3033
Shairyar Baig <shairyarbaig@gmail.com>
2022-05-23 18:17:30 +0500

    Initil commit without AppSignal instrumentation

--- app.js
+++ app.js
@@ -3,52 +3,32 @@ const http = require("http");
 const https = require("https");
 const tracer = appsignal.tracer();
 
+const axios = require('axios').default;
+
 const hostname = "127.0.0.1";
 const port = 3000;
 
-tracer.withSpan(tracer.createSpan(), span => {
-  span.setName("GET /");
-  const server = http.createServer((req, res) => {
-    tracer.withSpan(span.child(), (child) => {
-      child
-      .setName("GET /users")
-      .setCategory("process_request.http")
-      https
-        .get("https://jsonplaceholder.typicode.com/users", (res) => {
-          let data = [];
-          const headerDate =
-            res.headers && res.headers.date
-              ? res.headers.date
-              : "no response date";
-          console.log("Status Code:", res.statusCode);
-          console.log("Date in Response header:", headerDate);
+const server = http.createServer((req, res) => {
 
-          res.on("data", (chunk) => {
-            data.push(chunk);
-          });
-
-          res.on("end", () => {
-            console.log("Response ended: ");
-            const users = JSON.parse(Buffer.concat(data).toString());
-
-            for (user of users) {
-              console.log(`Got user with id: ${user.id}, name: ${user.name}`);
-            }
-          });
-        })
-        .on("error", (err) => {
-          console.log("Error: ", err.message);
-        });
-      child.close(); // don't forget to close the span if you're done with it!
+    // Make a request for a users
+    axios.get('https://reqres.in/api/users')
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
     });
 
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Hello World");
-    span.close(); // don't forget to close the span if you're done with it!
-  });
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
 
-  server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-  });
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
--- appsignal.js
+++ appsignal.js
@@ -3,8 +3,8 @@ const { Appsignal } = require("@appsignal/nodejs")
 exports.appsignal = new Appsignal({
   active: true,
   name: "Node Example",
-  apiKey: process.env.APPSIGNAL_PUSH_API_KEY,
+  push_api_key: process.env.APPSIGNAL_PUSH_API_KEY,
   environment: "development",
-  debug: true,
+  lov_level: "trace",
   logPath: "logs"
 })
