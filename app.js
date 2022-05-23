497da6d7821347939abd987d319539410a5518c1
Shairyar Baig <shairyarbaig@gmail.com>
2022-05-23 18:39:37 +0500

    Initil commit with AppSignal instrumentation

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
 const { appsignal } = require("./appsignal");
 const http = require("http");
-const https = require("https");
 const tracer = appsignal.tracer();
-
-const axios = require('axios').default;
+const axios = require("axios").default;
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-
+tracer.withSpan(tracer.createSpan(), (span) => {
+  span.setName("BackgroundWorker.perform");
+  const server = http.createServer((req, res) => {
     // Make a request for a users
-    axios.get('https://reqres.in/api/users')
-    .then(function (response) {
-      // handle success
-      console.log(response);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .then(function () {
-      // always executed
-    });
+    axios
+      .get("https://reqres.in/api/users")
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
 
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
-});
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello World");
+    span.close(); // don't forget to close the span if you're done with it!
+  });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
 });
